90bb97deafcb974331d167ec13cb8b9a3c37e525
Choharim <jhl8586@naver.com>
2020-12-05 23:18:23 +0900

    Add clock.js

---
+++ clock.js
@@ -0,0 +1,19 @@
+const clockContainer = document.querySelector(".clock-container"),
+clock = clockContainer.querySelector(".clock"),
+date = clockContainer.querySelector(".date");
+
+function getTime(){
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+  const currentMinute = currentTime.getMinutes();
+  const currentSecond = currentTime.getSeconds();
+  const currentDate = new Intl.DateTimeFormat('ko-KR').format(currentTime);
+
+  clock.innerText = `${currentHour <10 ? `0${currentHour}` : currentHour} : ${currentMinute <10? `0${currentMinute}` : currentMinute} : ${currentSecond <10 ? `0${currentSecond}` : currentSecond}`;
+  date.innerText=`at ${currentDate}`;
+}
+
+function init(){
+  setInterval(getTime,1000);
+}
+init();
