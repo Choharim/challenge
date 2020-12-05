804640255a4e736f7d2c609aab8b948bb36e2cf4
Choharim <jhl8586@naver.com>
2020-12-06 00:12:26 +0900

    Add showUserNameFn,modiNamingFn

--- name.js
+++ name.js
@@ -1,16 +1,30 @@
-const userNameContainer = document.querySelector(".userName-container");
+const userNameContainer = document.querySelector(".userName-container"),
+greetingContainer = userNameContainer.querySelector(".greeting"),
+userNameGreeting = greetingContainer.querySelector(".userName-greeting"),
+userNameModiBtn = greetingContainer.querySelector(".userName-modiBtn"),
+userNameForm = userNameContainer.querySelector(".userName-form"),
+userNameInput = userNameForm.querySelector(".userName-input");
 
 const USER_NAME = "userName";
 
 function showUserName(){
   const userName_LS = window.localStorage.getItem(USER_NAME);
   if(userName_LS){
-   
+    greetingContainer.classList.add("show");
+    userNameGreeting.innerText = `Welcome ${userName_LS} ^__^`;
+    userNameModiBtn.addEventListener("click",modiNaming);
   }else{
+    greetingContainer.classList.remove("show");
+    userNameForm.classList.add("show");
     saveUserName();
   }
 }
 
+function modiNaming(){
+  greetingContainer.classList.remove("show");
+  userNameForm.classList.add("show");
+  userNameInput.value = USER_NAME;
+}
 function saveUserName(){
 
 }
