c92f54f4e58a11f0db88479a365396c72369406a
Choharim <jhl8586@naver.com>
2020-12-05 23:56:43 +0900

    Modify html

--- name.js
+++ name.js
@@ -5,32 +5,16 @@ const USER_NAME = "userName";
 function showUserName(){
   const userName_LS = window.localStorage.getItem(USER_NAME);
   if(userName_LS){
-    const userNameGreeting = document.createElement("span");
-    const userNameModiBtn = document.createElement("button");
-
-    userNameContainer.appendChild(userNameGreeting);
-    userNameContainer.appendChild(userNameModiBtn);
-    userNameGreeting.classList.add("userName-greeting");
-    userNameModiBtn.classList.add("userName-modiBtn");
-    userNameModiBtn.innerHTML = '<i class="fas fa-eraser"></i>';
-
-    userNameGreeting.innerText = userName_LS;
+   
   }else{
     saveUserName();
   }
 }
 
 function saveUserName(){
-  const userNameForm = document.createElement("form");
-  const userNameInput = document.createElement("input");
-
-  userNameContainer.appendChild(userNameForm);
-  userNameContainer.appendChild(userNameInput);
-  userNameForm.classList.add("userName-form");
-  userNameInput.classList.add("userName-input");
-  userNameInput.placeholder = "What's your name?";
 
 }
+
 
 function init(){
   showUserName();
