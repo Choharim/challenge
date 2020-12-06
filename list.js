6420eb8db5649ab25136144a029b1a217505c77f
Choharim <jhl8586@naver.com>
2020-12-06 15:04:06 +0900

    Add initFn,submitListFormFn,showToDoFn

--- list.js
+++ list.js
@@ -5,8 +5,39 @@ listToDos = document.querySelector(".list-todos");
 const TODOS = "toDos";
 const toDosArray = [];
 
+function saveToDos(){
+  
+}
+
+function showToDo(todo){
+  const div = document.createElement("div");
+  const li = document.createElement("li");
+  const modiBtn =  document.createElement("button");
+  const delBtn =  document.createElement("button");
+
+  listToDos.appendChild(div);
+  div.appendChild(li);
+  div.appendChild(modiBtn);
+  div.appendChild(delBtn);
+
+  modiBtn.innerHTML = `<i class="fas fa-arrow-circle-left"></i>`;
+  delBtn.innerHTML = `<i class="far fa-times-circle"></i>`;
+
+  const todoObj = {
+    id : toDosArray.length + 1,
+    todo
+  };
+  toDosArray.push(todoObj);
+  saveToDos();
+}
+
 function submitListForm(event){
   event.preventDefault();
+  const listInputValue = listInput.value;
+
+  if(listInputValue !== ""){
+    showToDo(listInputValue);
+  }
 }
 
 function init(){
